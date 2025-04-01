33900dccec7394957c4396f23b343778a57c6ec9
Primus Vekuh <primus.vekuh@gmail.com>
2025-03-31 20:03:24 -0500

    fixed files

--- next.config.js
+++ next.config.js
@@ -17,6 +17,27 @@ const nextConfig = {
   experimental: {
     disableOptimizedLoading: false,
     optimizeCss: true
+  },
+  // Reduce output size further
+  output: 'standalone',
+  // Disable webpack5 caching to save disk space
+  webpack: (config, { dev, isServer }) => {
+    // Reduce chunk size
+    config.optimization.splitChunks = {
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 20000,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+            return `npm.${packageName.replace('@', '')}`;
+          },
+        },
+      },
+    };
+    return config;
   }
 }
 
